ac988b48498869634176a40662c35c9012048512
erenuenal@hotmail.de <erenuenal@hotmail.de>
2021-05-13 17:55:47 +0200

    Added "parsing.py", which contains objects for analyzing and converting XML formats

---
+++ parsing.py
@@ -0,0 +1,17 @@
+class get_articles():
+    """parsing for description in news"""
+    def parsing(self):
+        pass
+
+    def took_dataframe(self):
+        pass
+
+class Outputting():
+    """putting the data into a .txt file"""
+    def txt_output(self):
+        pass
+
+class Keywords():
+    """Search by keywords"""
+    def search(self):
+        pass
