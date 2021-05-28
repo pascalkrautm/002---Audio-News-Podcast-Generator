c88872f5024932ef5bc9908b4985a14f3855438f
erenuenal@hotmail.de <erenuenal@hotmail.de>
2021-05-28 19:19:46 +0200

    added new classes

---
+++ conversion.py
@@ -0,0 +1,19 @@
+class Convertor(object):
+    """Convert XML-file into txt-file"""
+    def parsing_articles(self):
+        pass
+
+    def into_dataframe(self):
+        pass
+
+
+class Cleanup(object):
+    """Clean txt-file for text to speach"""
+    def txt_output(self):
+        pass
+
+
+class Filter(object):
+    """Search XML-File by keywords"""
+    def search_keywords(self):
+        pass
