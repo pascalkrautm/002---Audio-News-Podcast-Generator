987a5ab8931a62c3b0a75ece46473840623f80e9
pascalkrautm <pascal.krautmacher@hs-duesseldorf.de>
2021-06-02 10:04:46 +0200

    rss_feed_scrapper.py updated

--- rss_feed_scrapper.py
+++ rss_feed_scrapper.py
@@ -14,20 +14,28 @@ feeds = []
 content = open("url_list.txt", "r")
 content = content.read()
 url_list = content.split(",")
-
+print(url_list)
 #User input
-#keyword = input("What topic are you interested in? Just type 'keyword' ('corona', 'soccer')")
+keyword = input("What topic are you interested in? Just type 'keyword' ('corona', 'soccer')")
+
+#liste leeren
+del feeds[:]
 
 #rss_feed_scrapper
 for url in url_list:
     feed = feedparser.parse(str(url))
     print("Starting scrap " + str(url))
     entries_len = len(feed.entries)
-    entry = feed.entries
     print(f"getting {entries_len} entries")
     print("searching for keyword")
-    if "corona" in entry:
-        feeds.append(feed.entry.title)
-        print("adding...")
-    else:
-        print("no entries found")
+    for entry in feed.entries:
+        if keyword in entry.title:
+            #testline
+            print(entry.title)
+            #testline
+            print(entry.summary)
+            feeds.append(entry.summary)
+        else:
+            pass
+
+print(feeds)
