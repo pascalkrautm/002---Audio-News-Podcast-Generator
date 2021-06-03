d412009163c09862803d4fe843a862ca2ae18bbb
Marco <marco.meinhold@googlemail.com>
2021-06-03 22:38:21 +0200

    Worked on Class Converter

--- createmp3.py
+++ createmp3.py
@@ -1,7 +1,9 @@
 import pyttsx3
 
-class Converter(Object): #Create class for the Object "Converter"
-    def __init__(self):
+engine = pyttsx3.init()
+
+class Converter(object): #Create class for the Object "Converter"
+    def __init__(self, file_name, text, rate, volume, voice):
         self.file_name = file_name
         self.text = text
         self.rate = rate
@@ -19,46 +21,17 @@ class Converter(Object): #Create class for the Object "Converter"
         engine.setProperty('volume', 1.0)  # setting up volume level  between 0 and 1
 
     def voice_option(self):
+
         voices = engine.getProperty('voices')  # getting details of current voice
         engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
         engine.setProperty('voice', voices[1].id)  # changing index, changes voices. 1 for female
+
+    def speak(self):
+        engine.say("Hello World")
+        engine.runAndWait()
 
     def saving_mp3(self):
         engine.save_to_file('Hello World', 'test.mp3')
         engine.runAndWait()
 
-#Noch zu verarbeiten
-engine = pyttsx3.init() # object creation
 
-engine.say("Hello World!")
-engine.say('My current speaking rate is ' + str(rate))
-engine.runAndWait()
-engine.stop()
-
-
-#Auszug aus dem Buch
-""" RATE"""
-rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
-engine.setProperty('rate', 125)     # setting up new voice rate
-
-
-"""VOLUME"""
-volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-print (volume)                          #printing current volume level
-engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
-
-"""VOICE"""
-voices = engine.getProperty('voices')       #getting details of current voice
-#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
-
-engine.say("Hello World!")
-engine.say('My current speaking rate is ' + str(rate))
-engine.runAndWait()
-engine.stop()
-
-"""Saving Voice to a file"""
-# On linux make sure that 'espeak' and 'ffmpeg' are installed
-engine.save_to_file('Hello World', 'test.mp3')
-engine.runAndWait()
