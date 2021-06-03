c7b8d2b576b125a7660a6ed4463c54476250c24d
erenuenal@hotmail.de <erenuenal@hotmail.de>
2021-06-03 18:37:19 +0200

    Added new Code

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-a = 1
+
+import feedparser
+import pandas as pd
+
+rssfeed = 'https://rss.sueddeutsche.de/rss/Topthemen'
+parsethefeed = feedparser.parse(rssfeed)
+
+df = pd.DataFrame()
+
+df['title'] = [post.title for post in parsethefeed.entries]
+df['description'] = [post.description for post in parsethefeed.entries]
+df['link'] = [post.link for post in parsethefeed.entries]
+print(df)
+
+df.to_csv('myfilename.csv', index=False)
