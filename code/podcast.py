2f04af38c94d6dcf15a7ede2a0cc164cbdf781fa
erenuenal@hotmail.de <erenuenal@hotmail.de>
2021-06-09 23:15:51 +0200

    Added new class with new objects

---
+++ code/podcast.py
@@ -0,0 +1,23 @@
+import re
+
+class Text:
+    def __init__(self,feeds_new:str):
+        self.feeds_new = feeds_new
+        self.feeds_clean = []
+        self.myText = mytext
+        self.myString = mystring
+
+    def clean(self):
+        feeds_new = str(self.feeds)
+        feeds_clean = str(re.findall("<p>(.*?)</p>", self.feeds_new, re.DOTALL))
+        print(feeds_clean)
+
+        return self.feeds_clean
+
+    def saver(self):
+        mytext = open(r'Feedoutputformp3.txt', 'w')
+        mystring = self.feeds_clean
+        mytext.write(mystring)
+        mytext.close()
+
+        return mytext.close()
