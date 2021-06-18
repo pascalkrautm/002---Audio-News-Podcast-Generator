de4501afa939e6580fbb7cc3ac6e43ba963e73ed
pascalkrautm <pascal.krautmacher@hs-duesseldorf.de>
2021-06-18 09:38:55 +0200

    updated

--- code/newsfeed.py
+++ code/newsfeed.py
@@ -7,6 +7,7 @@ class NewsfeedEntry:
     def __init__(self, title: str, summary: str, published: str, link: str):
         self.title = title
         self.summary = summary
+        self.clean_summary = clean_summary
         self.published = published
         self.link = link
 
@@ -21,11 +22,13 @@ class Newsfeed:
         self.title = feed.channel["title"]
         entries_len = len(feed.entries)
         keyword = input("What topic are you interested in? Just type 'keyword' ('corona', 'soccer')")
-        print(title)
+        #print(title)
         for entry in feed.entries:
             if keyword in entry.title:
-                self.entries.append(NewsfeedEntry(entry.title, entry.abstract, entry.published, entry.link))
+                self.clean_summary = re.sub("(<img.*?>)", "", entry.summary, 0, re.IGNORECASE | re.DOTALL | re.MULTILINE)
+                #self.entries.append(NewsfeedEntry(entry.title, entry.abstract, entry.published, entry.link))
+                self.feeds.append(NewsfeedEntry(feed.feed["title"], entry.published, entry.title, clean_summary))
             else:
                 pass
 
-        return self.entries
+        return self.feeds
