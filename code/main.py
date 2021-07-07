812ae6fb4a47e9889233483d8fcecda69955f19f
Marco <marco.meinhold@googlemail.com>
2021-07-07 14:58:02 +0200

    Updated user communication

--- code/main.py
+++ code/main.py
@@ -13,12 +13,22 @@ def main():
     #keyword = input("What topic are you interested in? Just type 'keyword' ('Corona', 'Fußball')")
     number_of_podcast = generator.generate_podcast(keywords)
     if number_of_podcast > 0:
-        voice_rate = Helper.get_voice_rate()
-        voice_volume = Helper.get_voice_volume()
-        voice_language = Helper.get_voice_language()
-        mp3_converter = Converter(rate=voice_rate, volume=voice_volume,language = voice_language)
-        mp3_converter.speak(generator.text)
-
+        ask_parameters = Helper.ask_parameters()
+        if ask_parameters == "n":
+            voice_rate = Helper.get_voice_rate()
+            voice_volume = Helper.get_voice_volume()
+            voice_language = Helper.get_voice_language()
+            ask_to_save_parameters = Helper.ask_to_save_parameter()
+            if ask_to_save_parameters == "y":
+                # Save parameters for next time.
+                pass
+            else:
+                pass
+            mp3_converter = Converter(rate=voice_rate, volume=voice_volume, language=voice_language)
+            mp3_converter.speak(generator.text)
+        else:
+            mp3_converter = Converter(rate=150, volume=1, language="german")
+            mp3_converter.speak(generator.text)
     else:
         print(f"No podcast with Keyword {keywords} found.")
 
