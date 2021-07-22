58550da89cf15fdbec4bcd7a9964bd8210772122
Marco <marco.meinhold@googlemail.com>
2021-07-22 17:30:49 +0200

    extends main.py Authors: Marco-Me, Matadore3

--- code/main.py
+++ code/main.py
@@ -7,6 +7,7 @@ from art import *
 
 print(text2art('''Podcast Gen''', font="small"))  # Multi-line print
 print("by Krautmacher - Ünal - Meinhold ")
+print("With this podcast generator you get an overview of the news that are interesting for you. You can either have them read to you immediately or download them for later as mp3 or pdf. ")
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
@@ -31,11 +32,9 @@ def main():
                 # work with the new parameters
                 mp3_converter = Converter(rate=save_parameters[0], volume=save_parameters[1],
                                           language=save_parameters[2])
-                mp3_converter.speak(generator.text)
             else:
                 pass
             mp3_converter = Converter(rate=voice_rate, volume=voice_volume, language=voice_language)
-            mp3_converter.speak(generator.text)
         else:
             # load saved parameters from last session
             open_file = open("parameters.pkl", "rb")
@@ -47,7 +46,23 @@ def main():
             # work with saved parameters
             mp3_converter = Converter(rate=voice_rate_default, volume=voice_volume_default,
                                       language=voice_language_default)
+
+        read_or_save = Helper.ask_read_or_save()
+        if read_or_save == "r":
+            print(generator.text)
             mp3_converter.speak(generator.text)
+
+        if read_or_save == "m":
+            mp3_file_name = Helper.ask_name_mp3()
+            mp3_converter.save_as_mp3(generator.text, f"{mp3_file_name}.mp3")
+
+        if read_or_save == "p":
+            pdf_file_name = Helper.ask_name_pdf()
+            mp3_converter.save_as_pdf(generator.text, f"{pdf_file_name}.pdf")
+
+        else:
+            mp3_converter.speak(generator.text)
+
     else:
         print(f"No podcast with Keyword {keywords} found.")
 
