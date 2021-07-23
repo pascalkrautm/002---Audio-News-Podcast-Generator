7e8045d583645c709e0fe98870a8e51ccad3f1ad
erenuenal@hotmail.de <erenuenal@hotmail.de>
2021-07-23 13:41:23 +0200

    updated def save_as_pdf Authors: Marco-Me, Matadore3, Pascalkrautm

--- code/mp3converter.py
+++ code/mp3converter.py
@@ -38,14 +38,36 @@ class Converter(object): #Create class for the Object "Converter"
         engine.runAndWait()
 
     def save_as_pdf(self, text:str, file_name:str = "Podcast.pdf"):
-        text = text.encode('utf-8')
+        import textwrap
+        from fpdf import FPDF
 
-        # using list comprehension
-        listToStr = ' '.join([str(elem) for elem in text])
+        text = text.replace("'", "").replace(" – ", " ").replace("[", "").replace("]", "").replace("’", "")\
+            .replace("'", "")
+        text.encode("utf-8")
 
-        pdf = FPDF()
+        a4_width_mm = 210
+        pt_to_mm = 0.35
+        fontsize_pt = 10
+        fontsize_mm = fontsize_pt * pt_to_mm
+        margin_bottom_mm = 10
+        character_width_mm = 7 * pt_to_mm
+        width_text = a4_width_mm / character_width_mm
+
+        pdf = FPDF(orientation='P', unit='mm', format='A4')
+        pdf.set_auto_page_break(True, margin=margin_bottom_mm)
         pdf.add_page()
-        pdf.set_font("Arial", size=12)
-        pdf.cell(200, 10, txt=listToStr, ln=1, align='C')
-        pdf.output(file_name)
+        pdf.set_font(family='Arial', size=fontsize_pt)
+        splitted = text.split('\n')
+
+        for line in splitted:
+            lines = textwrap.wrap(line, width_text)
+
+            if len(lines) == 0:
+                pdf.ln()
+
+            for wrap in lines:
+                pdf.cell(0, fontsize_mm, wrap, ln=1)
+
+            pdf.output(file_name, 'F')
+            print(text)
 
